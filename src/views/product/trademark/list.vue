bc8192b6b85c6f2c062d68de87abbf4527ac0572
qiuQiu <qiulan316@163.com>
2020-12-11 13:14:41 +0800

    feat:品牌页面展示

--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -1,11 +1,54 @@
 <template>
-<div>
-  品牌管理
-</div>
+  <div>
+    <el-button type="primary" icon="el-icon-plus">默认按钮</el-button>
+    <el-table :data="tableData" border style="width: 100%; margin: 20px 0">
+      <el-table-column prop="id" label="序号" width="80" align="center">
+      </el-table-column>
+      <el-table-column prop="name" label="品牌名称"> </el-table-column>
+      <el-table-column label="品牌LOGO">
+        <template slot-scope="scope">
+          <img class="trademark-img" :src="scope.row.logo" alt="logo" />
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <el-button type="warning" icon="el-icon-edit">修改</el-button>
+        <el-button type="danger" icon="el-icon-delete">删除</el-button>
+      </el-table-column>
+    </el-table>
+    <!-- 分页器 -->
+    <el-pagination
+      class="trademark-pagination"
+      :page-sizes="[3, 6, 9]"
+      :page-size="3"
+      layout=" prev, pager, next, jumper, sizes,total"
+      :total="13"
+    >
+    </el-pagination>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'TrademarkList',
-}
+  name: "TrademarkList",
+  data() {
+    return {
+      tableData: [
+        {
+          id: 1,
+          name: "iphone",
+          logo:
+            "https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1819216937,2118754409&fm=26&gp=0.jpg",
+        },
+      ],
+    };
+  },
+};
 </script>
+<style lang="sass" >
+.trademark-img
+  width: 150px
+.trademark-pagination
+  text-align: right
+.el-pagination__sizes
+  margin-left: 250px
+</style>
