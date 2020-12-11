0084c827c3dbad56e0539b3916f62fa31fd0d803
qiuQiu <qiulan316@163.com>
2020-12-11 21:22:15 +0800

    feat:form表单

--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <el-button type="primary" icon="el-icon-plus">默认按钮</el-button>
+    <el-button type="primary" icon="el-icon-plus" @click="visible = true"
+      >添加</el-button
+    >
     <el-table :data="trademarkList" border style="width: 100%; margin: 20px 0">
       <el-table-column type="index" label="序号" width="80" align="center">
       </el-table-column>
@@ -44,6 +46,43 @@
       :total="total"
     >
     </el-pagination>
+    <!-- 上传图片from表单 -->
+    <el-dialog title="添加品牌" :visible.sync="visible">
+      <el-form
+        :model="trademarkForm"
+        :rules="rules"
+        ref="trademarkForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="品牌名称" prop="tmName">
+          <el-input v-model="trademarkForm.tmName" style="width: 450px"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" prop="logoUrl">
+          <el-upload
+            class="avatar-uploader"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img
+              v-if="trademarkForm.logoUrl"
+              :src="trademarkForm.logoUrl"
+              class="avatar"
+            />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
+          <span style="font-size: 12px">只能上传jpg/png文件，且不超过50kb</span>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="visible = false">取 消</el-button>
+        <el-button type="primary" @click="submitForm('trademarkForm')"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -64,6 +103,22 @@ export default {
       total: 0,
       page: 1, // 默认当前页码
       limit: 3, // 默认选择的每页显示3条
+      visible: false, // 默认隐藏上传的弹框
+      // 上传图片表单数据
+      trademarkForm: {
+        tmName: "",
+        logoUrl: "",
+      },
+      // 表单的校验规则
+      rules: {
+        tmName: [
+          { required: true, message: "请输入品牌名称", trigger: "blur" },
+        ],
+        logoUrl: [
+          { required: true, message: "请上传品牌LOGO图片", trigger: "change" },
+        ],
+      },
+      imageUrl: "",
     };
   },
   // 方法
@@ -99,6 +154,32 @@ export default {
         this.$message.error("获取品牌分页列表失败");
       }
     },
+
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    },
+    // 提交表单校验
+    submitForm(form) {
+      this.$refs[form].validate((valid) => {
+        if (valid) {
+          console.log(this.trademarkFrom);
+        } else {
+          console.log("表单校验失败");
+        }
+      });
+    },
   },
   mounted() {
     //  console.log(this);
@@ -106,11 +187,33 @@ export default {
   },
 };
 </script>
-<style lang="sass" >
+<style lang="sass" scoped>
 .trademark-img
   width: 150px
+
 .trademark-pagination
   text-align: right
-.el-pagination__sizes
+
+>>>.el-pagination__sizes
   margin-left: 250px
+
+>>>.avatar-uploader .el-upload
+  border: 1px dashed #d9d9d9
+  border-radius: 6px
+  cursor: pointer
+  position: relative
+  overflow: hidden
+  &:hover
+    border-color: #409EFF
+>>>.avatar-uploader-icon
+  font-size: 28px
+  color: #8c939d
+  width: 178px
+  height: 178px
+  line-height: 178px
+  text-align: center
+>>>.avatar
+  width: 178px
+  height: 178px
+  display: block
 </style>
